bf0c326ea3d41d24d181798549e7a1222b828af1
Bill Ritchie <billritchie@Bill-Ritchies-iMac.local>
2012-04-02 15:49:01 -0400

    Added hadoop.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [cascalog/cascalog "1.9.0-wip8"]])
+                 [cascalog/cascalog "1.9.0-wip8"]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]])
